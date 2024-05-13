f2e44815ea7306707b3dfbdbc262afeb0aad371f
ajit3676 <167732185+ajit3676@users.noreply.github.com>
2024-05-13 15:59:15 +1000

    code change

---
+++ NewGroupProject/Migrations/202405101412063_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace NewGroupProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        ReservationID = c.Int(nullable: false, identity: true),
+                        ReservationName = c.String(),
+                        NoofGuests = c.Int(nullable: false),
+                        ReservationDate = c.String(),
+                        ReservationTime = c.String(),
+                    })
+                .PrimaryKey(t => t.ReservationID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Reservations");
+        }
+    }
+}
---
+++ NewGroupProject/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace NewGroupProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NewGroupProject.Data.NewGroupProjectContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "NewGroupProject.Data.NewGroupProjectContext";
+        }
+
+        protected override void Seed(NewGroupProject.Data.NewGroupProjectContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
