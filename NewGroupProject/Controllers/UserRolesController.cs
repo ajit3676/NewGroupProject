d60cafe06c04e2ec66c5606ca97984098eab6e3a
ajit3676 <167732185+ajit3676@users.noreply.github.com>
2024-05-13 16:38:05 +1000

    changes today 13/05/2024

---
+++ NewGroupProject/Controllers/UserRolesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NewGroupProject.Data;
+using NewGroupProject.Models;
+
+namespace FormAuthRoles.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: UserRoles
+        public ActionResult Index()
+        {
+            var userRoles = db.UserRoles.Include(u => u.Role).Include(u => u.User);
+            return View(userRoles.ToList());
+        }
+
+        // GET: UserRoles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoles userRoles = db.UserRoles.Find(id);
+            if (userRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoleID = new SelectList(db.Roles, "Id", "RoleName");
+            ViewBag.UserID = new SelectList(db.Users, "Id", "UserName");
+            return View();
+        }
+
+        // POST: UserRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserID,RoleID")] UserRoles userRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserRoles.Add(userRoles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleID = new SelectList(db.Roles, "Id", "RoleName", userRoles.RoleID);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "UserName", userRoles.UserID);
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoles userRoles = db.UserRoles.Find(id);
+            if (userRoles == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "Id", "RoleName", userRoles.RoleID);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "UserName", userRoles.UserID);
+            return View(userRoles);
+        }
+
+        // POST: UserRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserID,RoleID")] UserRoles userRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userRoles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "Id", "RoleName", userRoles.RoleID);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "UserName", userRoles.UserID);
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserRoles userRoles = db.UserRoles.Find(id);
+            if (userRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userRoles);
+        }
+
+        // POST: UserRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserRoles userRoles = db.UserRoles.Find(id);
+            db.UserRoles.Remove(userRoles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NewGroupProject/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewGroupProject.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public virtual ICollection<UserRoles> UserRoles { get; set; }
+    }
+
+}
